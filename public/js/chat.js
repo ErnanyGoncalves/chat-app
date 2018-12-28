cbc115c7f5786aa4dba232b6248ce05acb79f8f5
Ernany Gonçalves <goncalves.ernany@gmail.com>
2018-12-28 17:24:25 -0200

    Add user class and updateserverlist event

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,6 @@
 var socket = io();
 
-function scrollToBottom(){
+function scrollToBottom() {
     var messages = jQuery("#messages");
     var newMessage = messages.children("li:last-child")
 
@@ -10,7 +10,7 @@ function scrollToBottom(){
     var newMessageHeight = newMessage.innerHeight();
     var lastMessageHeight = newMessage.prev().innerHeight();
 
-    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
         messages.scrollTop(scrollHeight);
     }
 
@@ -18,7 +18,16 @@ function scrollToBottom(){
 
 
 socket.on("connect", function () {
-    console.log("Connected to server");
+    var params = jQuery.deparam(window.location.search);
+
+    socket.emit('join', params, function (err) {
+        if (err) {
+            alert(err);
+            window.location.href = '/';
+        } else {
+            console.log('No error');
+        }
+    });
 
 });
 
@@ -26,6 +35,14 @@ socket.on("disconnect", function () {
     console.log("Disconnected from server");
 });
 
+
+socket.on('updateUserList', function (users) {
+    var ol = jQuery('<ol></ol>');
+    users.forEach(function(user){
+        ol.append(jQuery('<li></li>').text(user));
+    });
+    jQuery('#users').html(ol);
+});
 
 socket.on("newMessage", function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
